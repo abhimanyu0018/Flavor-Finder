f7c55fda8bf47485e0bcb00b2a56c72a0dec4e8c
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-05-18 20:51:41 +0530

    database connection

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,10 @@
 import express from "express"
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv"
 
 const app = express()
 
-dotenv.config(
-    {
-        path: "../env"
-    }
-)
+
 
 app.use(
     cors({
@@ -22,7 +17,6 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser());
 
 
 app.get('/', (req,res) => { console.log("hello")})
@@ -32,4 +26,4 @@ app.listen(8000, () => {
     console.log(`Server is running at port :8000 ` );
 })
 
-// export default app
+export default app
