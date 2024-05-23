7d9293e2cfe570e3c8ea020101b456505fca28af
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-05-23 12:36:29 +0530

    added a feature to get random recipe

--- server/src/controllers/recipe.controller.js
+++ server/src/controllers/recipe.controller.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 
+
+// controller for recipe search by name --
 export const searchRecipeController = async (req, res) => {
     try {
         const { recipeName } = req.body;
@@ -25,6 +27,26 @@ export const searchRecipeController = async (req, res) => {
         return res.status(200).json({ success: true, data: data.meals });
     } catch (error) {
         console.error('Error searching recipe:', error.message);
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+
+// controller for fetch random recipe --
+export const getRandomRecipe = async (req,res) => {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data from API. Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return res.status(200).json({success: true, data: data.meals})
+        
+    } catch (error) {
+        console.error('Error in fetching random recipe:', error.message);
         return res.status(500).json({ success: false, error: error.message });
     }
 }
--- server/src/routes/recipe.routes.js
+++ server/src/routes/recipe.routes.js
@@ -1,12 +1,18 @@
 import express from "express"
-import { searchRecipeController } from "../controllers/recipe.controller.js"
+import { getRandomRecipe, searchRecipeController } from "../controllers/recipe.controller.js"
 
 
 
 const recipeRouter = express.Router()
 
 
+//route to get random recipe --
+recipeRouter.get('/random-recipe', getRandomRecipe)
+
+// route to search recipe by name --
 recipeRouter.post('/search', searchRecipeController)
+
+
 
 
 
